20aad657b712b6c6917f87c67f598786ae41250d
yuyuyzl <yuzhongliang221@gmail.com>
2017-07-25 11:17:47 +0800

    algorithm rebuild -> much faster

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
         var r = parseInt($("#inputR").val());
         readConfig();
         var arrCircle = genCircleArray(r);
-        var htmlCircleTable = "<table><tbody>";
+        var htmlCircleTable = "<table cellspacing='0' border='1px'><tbody>";
         var hullCount = 0;
         var tunnelCount = 0;
         var data1=calculateMinEnergy(r,arrCircle,0);
@@ -76,17 +76,50 @@ var genCircleArray=function (r) {
         arrCircle[i] = new Array(r * 2 + 5);
     }
     var c = r + 2.5;
-
+    /*
     for (var i = 0; i < 360; i += 0.0011) {
         arrCircle[Math.floor(c + r * Math.sin(i))][Math.floor(c + r * Math.cos(i))] = 2;
+    }*/
+
+    var px=2,py=r+2;
+    var modified=0;
+    var dirOffset=[[1,0],[0,1],[-1,0],[0,-1]];
+    var dirfrom=0;
+    //console.log("---------------");
+    for(var t=0;t<r*8;t++){
+        //console.log(px+" "+py);
+        arrCircle[px][py]=2;
+        var mindir=-1,mindis=1e15;
+        for(var ii=dirfrom+3;ii<dirfrom+3+4;ii++){
+            var i=ii%4;
+            if(px+dirOffset[i][0]<0)continue;
+            if(px+dirOffset[i][0]>=r*2+5)continue;
+            if(py+dirOffset[i][1]<0)continue;
+            if(py+dirOffset[i][1]>=r*2+5)continue;
+            if (arrCircle[px+dirOffset[i][0]][py+dirOffset[i][1]]==2)continue;
+            var dis=Math.abs(Math.pow(px+dirOffset[i][0]+0.5-c,2)+Math.pow(py+dirOffset[i][1]+0.5-c,2)-r*r);
+            if(mindis>dis){
+                mindis=dis;
+                mindir=i;
+            }
+        }
+        px+=dirOffset[mindir][0];
+        py+=dirOffset[mindir][1];
+        modified++;
+        dirfrom=mindir;
     }
 
+
+
     for (var i = 0; i < r * 2 + 5; i++) {
+        //var s="";
         for (var j = 0; j < r * 2 + 5; j++) {
+            //s+=Math.abs(Math.pow(i+0.5-c,2)+Math.pow(j+0.5-c,2)-r*r)+"\t";
             if (!arrCircle[i][j] || arrCircle[i][j] != 2) {
                 if ((i + 1 < r * 2 + 5 && arrCircle[i + 1][j] == 2) || (i - 1 >= 0 && arrCircle[i - 1][j] == 2) || arrCircle[i][j + 1] == 2 || arrCircle[i][j - 1] == 2) arrCircle[i][j] = 1; else arrCircle[i][j] = 0;
             }
         }
+        //console.log(s);
     }
     arrCircle[Math.floor(c)][Math.floor(c)] = 3;
     return (arrCircle);
@@ -148,16 +181,31 @@ var calculateMinEnergy=function (r,arrCircle,stabNum) {
     }
     if (rt<0)rt=0;
     if(stabNum<0)rt=0;
-    for (var i = 1; i <= 100000; i += 1) {
+
+    var getans=function (i) {
+
         var a = calculateAcceleration(property.drag, i, Config.kAcceleration, Config.kOverall, 0, rt, 0);
         //var a2 = calculateAcceleration(property.drag, i, Config.kAcceleration, Config.kOverall, 0, 0, 0);
         var t = 100 / a;
         var totEU = t * i;
-        if (totEU > 0 && totEU < min) {
-            min = totEU;
-            minE = i;
+        totEU=Math.round(totEU);
+        return totEU>0?totEU:1e15;
+    };
+
+    var triSearch=function (l,r) {
+        if(r-l<1){
+            return{minE:Math.round(l),min:getans(l)}
         }
-    }
-    min=Math.round(min);
-    return {property:property,min:min,minE:minE};
+        var mid=l+r;
+        mid/=2;
+        var midmid=mid+r;
+        midmid/=2;
+        var midans=getans(mid);
+        var midmidans=getans(midmid);
+        if (midmidans<midans)return triSearch(mid,r);
+        else return triSearch(l,midmid);
+
+    };
+    var a=triSearch(1,1000000);
+    return {property:property,min:a.min,minE:a.minE};
 };
